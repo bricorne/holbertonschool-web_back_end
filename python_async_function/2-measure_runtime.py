22a6c9d3b965ceee555408ea845816b2f4a25fd6
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-09-01 19:58:15 +0200

    task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the runtime"""
+    start = time.process_time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.process_time()
+    return (end - start) / n
