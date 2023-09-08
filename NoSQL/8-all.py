c2848daa4927ab826690b7fa510c025bbac0bd2a
bricorne <bricepoitevineau@gmail.com>
2023-09-08 09:00:34 -0700

    task 8-12

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,2 +1,10 @@
-//Write a Python function that lists all documents in a collection
+#!/usr/bin/env python3
+"List all documents in Python"""
+import pymongo
 
+
+def list_all(mongo_collection):
+    """function that lists all documents in a collection"""
+    if mongo_collection.count() == 0:
+        return []
+    return mongo_collection.find()
