38fe1a4ea7620454ddc9828588ae6495139aaa5a
bricorne <bricepoitevineau@gmail.com>
2023-09-08 16:36:30 +0000

    pyco

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """The function should return a tuple of size two containing a start index"""
+    """The function should return a tuple of size two containing a index"""
     start = (page - 1) * page_size
     end = page * page_size
     return(start, end)
