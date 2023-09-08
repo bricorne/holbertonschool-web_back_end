d6b77af33655f41ba6c7190488a67ce6455ed1b2
bricorne <bricepoitevineau@gmail.com>
2023-09-08 16:33:07 +0000

    task 0123

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""1. Simple pagination"""
+"""2.Hypermedia pagination"""
 import csv
-import math
-from typing import Tuple, List
+from math import ceil
+from typing import Tuple, List, Dict, Union
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """The function should return a tuple of size two containing a start index
     and an end index corresponding to the range of indexes to return in a list
-    for those particular pagination parameters."""
+    for those particular pagination parameters"""
     start = (page - 1) * page_size
     end = page * page_size
     return(start, end)
@@ -46,3 +46,28 @@ class Server:
         if end > len(self.dataset()):
             return []
         return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """returns a dictionary containing data set pagination info"""
+        data = self.get_page(page, page_size)
+
+        if self.get_page(page + 1, page_size) == []:
+            next_page = None
+        else:
+            next_page = (page + 1)
+
+        if page == 1:
+            prev_page = None
+        else:
+            prev_page = (page - 1)
+
+        total_pages = ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
