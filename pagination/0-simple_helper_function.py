1767c4c19f7b7a49151fcd076f94f72a57b6c9b2
bricorne <bricepoitevineau@gmail.com>
2023-09-08 09:25:47 -0700

    task 1-2

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""0. Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """The function should return a tuple of size two containing a start index"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return(start, end)
